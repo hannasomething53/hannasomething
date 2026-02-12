4c8668431cc27b5c6614c7a0c809504267be52d1
hannasomething53 <hannasomething53@gmail.com>
2026-02-12 16:12:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-document.querySelectorAll('.side-nav a').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({
-      behavior: 'smooth'
+document.addEventListener("DOMContentLoaded", function () {
+
+  const images = document.querySelectorAll(".zoomable");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  images.forEach(img => {
+    img.addEventListener("click", function () {
+      lightbox.style.display = "flex";
+      lightboxImg.src = this.src;
     });
   });
-});
 
+  lightbox.addEventListener("click", function () {
+    lightbox.style.display = "none";
+  });
+
+});
