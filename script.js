02b33f4fca1ab0c169624bdd8f0cc8710d11d078
hannasomething53 <hannasomething53@gmail.com>
2026-02-12 17:06:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
+const viewer = document.getElementById("viewer");
+const viewerImg = document.getElementById("viewer-img");
 
-    const images = document.querySelectorAll(".zoomable");
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImg = document.getElementById("lightbox-img");
 
-    // 이미지 클릭 → 확대
-    images.forEach(img => {
-        img.addEventListener("click", function () {
-            lightbox.style.display = "flex";
-            lightboxImg.src = this.src;
-        });
-    });
 
-    // 배경 클릭 → 닫기
-    lightbox.addEventListener("click", function () {
-        lightbox.style.display = "none";
-    });
+/* 이미지 클릭 확대 */
+
+document.querySelectorAll(".art").forEach(img=>{
+img.addEventListener("click",()=>{
+viewer.style.display="flex";
+viewerImg.src = img.src;
+});
+});
+
+
+
+/* 배경 클릭 닫기 */
+
+viewer.addEventListener("click",()=>{
+viewer.style.display="none";
+});
+
+
+
+/* 썸네일 이동 */
+
+document.querySelectorAll(".right-nav img").forEach(thumb=>{
+
+thumb.addEventListener("click",()=>{
+
+const id = thumb.dataset.target;
+document.getElementById(id).scrollIntoView({
+behavior:"smooth",
+block:"start"
+});
+
+});
 
 });
