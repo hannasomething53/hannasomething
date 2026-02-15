0bc2abe7cbdee78eb89aaa99f60c38959c72802d
hannasomething53 <hannasomething53@gmail.com>
2026-02-15 22:24:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const gallery = document.getElementById("gallery");
 
-for (let i = 1; i <= 22; i++) {
+for(let i=1;i<=22;i++){
 
     const img = document.createElement("img");
     img.src = `images/e${i}.jpg`;
-    img.alt = `e${i}`;
 
-    img.onerror = function () {
-        const div = document.createElement("div");
-        div.className = "broken";
-        div.textContent = `이미지 없음: e${i}.jpg`;
-        img.replaceWith(div);
-    };
+    img.onclick = () => openLightbox(img.src);
 
     gallery.appendChild(img);
+}
+
+/* ===== LIGHTBOX ===== */
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+function openLightbox(src){
+
+    lightbox.style.display="flex";
+    lightboxImg.src = src;
+
+    // viewport 기준 자동 맞춤
+    const vw = window.innerWidth * 0.9;
+    const vh = window.innerHeight * 0.95;
+
+    lightboxImg.style.maxWidth =
+        Math.min(1200, vw) + "px";
+
+    lightboxImg.style.maxHeight = vh + "px";
+}
+
+lightbox.onclick = () => {
+    lightbox.style.display="none";
 }
