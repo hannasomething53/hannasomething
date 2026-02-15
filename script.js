50b71bab3475ff8b8fc127c052cc05716f0b989f
hannasomething53 <hannasomething53@gmail.com>
2026-02-15 20:39:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const imgs = document.querySelectorAll(".art");
-const lightbox = document.getElementById("lightbox");
-const lbImg = document.getElementById("lightbox-img");
+const gallery = document.getElementById("gallery");
 
-imgs.forEach(img=>{
-  img.addEventListener("click",()=>{
-    lbImg.src = img.src;
-    lightbox.classList.add("show");
-  });
-});
+for (let i = 1; i <= 22; i++) {
 
-lightbox.addEventListener("click",()=>{
-  lightbox.classList.remove("show");
-});
+    const img = document.createElement("img");
+    img.src = `images/e${i}.jpg`;
+    img.alt = `e${i}`;
+
+    img.onerror = function () {
+        const div = document.createElement("div");
+        div.className = "broken";
+        div.textContent = `이미지 없음: e${i}.jpg`;
+        img.replaceWith(div);
+    };
+
+    gallery.appendChild(img);
+}
